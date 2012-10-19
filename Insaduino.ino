5b1787a532db167810023fb9743ac782e82a03c7
perschulte <per.schulte@gmail.com>
2012-10-19 18:17:55 +0300

    reworked led control

--- Insaduino.ino
+++ Insaduino.ino
@@ -1,171 +1,210 @@
- 
-#define DEBUG 1
-#define COMMAND_BUFFER_SIZE 100
 
 byte mode=0;
 
 //RGB-LED 1
-int  L1_Red    = 3;
+int  L1_Red    = 6;
 int  L1_Blue   = 5;
-int  L1_Green  = 6;
+int  L1_Green  = 3;
 
 //RGB-LED 2
-int  L2_Red    = 9;
+int  L2_Red    = 11;
 int  L2_Blue   = 10;
-int  L2_Green  = 11;
+int  L2_Green  = 9;
 
 //White-LED 1
 int L1_White   = 2;
 
 //White-LED 2
-int L2_White   = A6;
+int L2_White   = A5;
 
 //Photoresistor
 int S1_Light = A0;
 
-int red = 3;
-int green = 6;
-int blue = 5;
-int white = 9;
+//preset colors
+byte color1[] = {255, 19, 0}; 
+byte color2[] = {255, 133, 0}; 
+byte color3[] = {201, 0, 133};
+byte color4[] = {0, 195, 34};
 
-byte pink[] = {255,20,147}; 
+// color targets
+byte L1_Red_Target     = 0;
+byte L1_Green_Target   = 0; 
+byte L1_Blue_Target    = 0;
 
-byte redLed1 = 0;
-byte greenLed1 = 0;
-byte blueLed1 = 0;
+byte L2_Red_Target     = 0;
+byte L2_Green_Target   = 0; 
+byte L2_Blue_Target    = 0;
 
-byte rndRed;
-byte rndGreen;
-byte rndBlue;
+byte L1_White_Target     = 0;
+byte L2_White_Target     = 0; 
+
+//color states
+byte L1_Red_State     = 0;
+byte L1_Green_State   = 0; 
+byte L1_Blue_State    = 0;
+
+byte L2_Red_State     = 0;
+byte L2_Green_State   = 0; 
+byte L2_Blue_State    = 0;
+
+byte L1_White_State     = 0;
+byte L2_White_State     = 0;
+
+int millisSinceLastMesssage=0;
 
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(L2_White, OUTPUT);
-  //pinMode(green, OUTPUT);
-  //pinMode(blue, OUTPUT);
-  //pinMode(white, OUTPUT);
+  pinMode(L1_Red, OUTPUT);
+  pinMode(L1_Blue, OUTPUT);
+  pinMode(L1_Green, OUTPUT);
+  pinMode(L2_Red, OUTPUT);
+  pinMode(L2_Blue, OUTPUT);
+  pinMode(L2_Green, OUTPUT);
+  pinMode(L1_White, OUTPUT);
   
   Serial.begin(9600);
   randomSeed(analogRead(0));
-  digitalWrite(L2_White, HIGH);
-  
+ 
+  //digitalWrite(L2_White, HIGH);
+  //digitalWrite(L1_Red, HIGH);
+  //digitalWrite(L1_Blue, HIGH);
+  //digitalWrite(L1_Green, HIGH);
+  //digitalWrite(L2_Red, HIGH);
+  //digitalWrite(L2_Blue, HIGH);
+  //digitalWrite(L2_Green, HIGH);
+  //digitalWrite(L1_White, HIGH);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
-  //analogWrite(L1_Blue, analogRead(S1_Light));
-  analogWrite(L1_Red, analogRead(S1_Light));
-/*
+
+
   if (Serial.available() > 0) {
+    millisSinceLastMesssage = millis();
+    Serial.println("received serial data");
     mode = Serial.read();
-    if(mode == 1) 
-    { 
-      newMessage();
-      Serial.println("newMessage");
-      Serial.println(mode);
-    }
-    if(mode == 0) 
+    Serial.println(mode);
+    if(mode == 'm')
     {
-      blueLight();
-      Serial.println("idle");
-      Serial.println(mode);
+      mode = Serial.read();
+      if(mode == 1) 
+      { 
+        newMessage();
+        Serial.println("Status->new message");
+      }
+      if(mode == 2) 
+      { 
+        newMessage();
+        Serial.println("Status->unread mails");
+      }
+      
+      if(mode == 0) 
+      {
+        idle();
+        Serial.println("Status->idle");
+      }   
     }
-  } else if (mode == 0) {
-    blueLight();
-  }
-  */
-}
-
-char* readCommand()
-{
-  char commandBuffer[COMMAND_BUFFER_SIZE];     //CommandBuffer
-  int i=0;                     //Counter
-  
-  if(Serial.available()){
-     delay(10);
-     while( Serial.available() && i< (COMMAND_BUFFER_SIZE - 1)) {
-        commandBuffer[i++] = Serial.read();
-     }
-     commandBuffer[i++]='\0';
-  }
-
-#ifdef DEBUG
-  Serial.println("Command received:");
-  Serial.println((char*)commandBuffer);
-#endif
-
-  return commandBuffer;
-}
-
-
-
-
-
-
-
-boolean setMode()
-{
-  byte input[5];     //
-  int i;             //counter
-  if (Serial.available() > 0) 
-  {
-    while(Serial.available())
-    {
-      input[i] = Serial.read();
-    }
-    
-    
   } else {
-    return false;
+    Serial.println("no serial data");
+    Serial.println("Status->idle");
+    
   }
-}
-
-void redLight() 
-{
-  analogWrite(red, 255);
-  analogWrite(green, 0);
-  analogWrite(blue, 0);
-  analogWrite(white, 0);
-}
-void greenLight() 
-{
-  analogWrite(red, 0);
-  analogWrite(green, 255);
-  analogWrite(blue, 0);
-  analogWrite(white, 0);
-}
-void blueLight() 
-{
-  analogWrite(red, 0);
-  analogWrite(green, 0);
-  analogWrite(blue, 255);
-  analogWrite(white, 0);
+  
+  if ((millis() - millisSinceLastMesssage) > 10000) 
+  {
+    idle();
+  }
+    
+  //changeColors
+  
 }
 
 void idle()
 {
-  rndRed = random(-10,10);
-  rndGreen = random(-10,10);
-  rndBlue = random(-10,10);
+  //setTargetColors
+  L1_Red_Target     = color1[0];
+  L1_Green_Target   = color1[1]; 
+  L1_Blue_Target    = color1[2];
+
+  L2_Red_Target     = color1[0];
+  L2_Green_Target   = color1[1]; 
+  L2_Blue_Target    = color1[2];
+
+  L1_White_Target     = 255;
+  L2_White_Target     = 255;
   
-  redLed1 = redLed1 + rndRed;
-  greenLed1 = greenLed1 + rndGreen;
-  blueLed1 = blueLed1 + rndGreen;
+  if (checkLightSensor
   
-  analogWrite(red, redLed1);
-  analogWrite(green, greenLed1);
-  analogWrite(blue, blueLed1);
-  analogWrite(white, 128);
+  /*
+  analogWrite(L1_Red, color1[0]);
+  analogWrite(L1_Green, color1[1]);
+  analogWrite(L1_Blue, color1[2]);
+    
+  analogWrite(L2_Red, color1[0]);
+  analogWrite(L2_Green, color1[1]);
+  analogWrite(L2_Blue, color1[2]);
+  */
+}
+
+void noSignal()
+{ 
+  //check light sensor
   
-  delay(10);
+  //setTargetColors
+  L1_Red_Target     = 0;
+  L1_Green_Target   = 0; 
+  L1_Blue_Target    = 0;
+
+  L2_Red_Target     = 0;
+  L2_Green_Target   = 0; 
+  L2_Blue_Target    = 0;
+
+  L1_White_Target     = 0;
+  L2_White_Target     = 0;
+  
+  analogWrite(L1_Red, color1[0]);
+   analogWrite(L1_Green, color1[1]);
+   analogWrite(L1_Blue, color1[2]);
+    
+   analogWrite(L2_Red, color1[0]);
+   analogWrite(L2_Green, color1[1]);
+   analogWrite(L2_Blue, color1[2]);
+   
 }
 
 void newMessage() 
 {
-    analogWrite(white, 0);
-    analogWrite(red, pink[0]);
-    analogWrite(green, pink[1]);
-    analogWrite(blue, pink[2]);
-    delay(100);
+    analogWrite(L1_Red, color4[0]);
+    analogWrite(L1_Green, color4[1]);
+    analogWrite(L1_Blue, color4[2]);
+    
+    analogWrite(L2_Red, color4[0]);
+    analogWrite(L2_Green, color4[1]);
+    analogWrite(L2_Blue, color4[2]);
+}
+
+byte checkLightSensor()
+{
+  //set LEDs to zero for 10ms
+  analogWrite(L1_Red, 0);
+  analogWrite(L1_Green, 0);
+  analogWrite(L1_Blue, 0);
+  analogWrite(L2_Red, 0);
+  analogWrite(L2_Green, 0);
+  analogWrite(L2_Blue, 0);
+  analogWrite(L1_White, 0);
+  analogWrite(L2_White, 0);
+  
+  byte result = analogRead(S1_Light);
+  
+  //set LEDs back to previous state
+  analogWrite(L1_Red, L1_Red_State);
+  analogWrite(L1_Green, L1_Green_State);
+  analogWrite(L1_Blue, L1_Blue_State);
+  analogWrite(L2_Red, L1_Red_State);
+  analogWrite(L2_Green, L1_Green_State);
+  analogWrite(L2_Blue, L1_Blue_State);
+  analogWrite(L1_White, L1_White_State);
+  analogWrite(L2_White, L2_White_State);
 }
